913e4ef62eb66802508d4e7eefcc7e34b8ea57ee
Brandon Liebman <bliebman75@gmail.com>
2015-10-19 21:20:36 -0400

    Initial Commit

---
+++ Corners/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Corners
+//
+
+//  Copyright (c) 2015 Brandon Liebman. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
